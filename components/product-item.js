d2bf21fb9174aee7c0254f28be098e29f2a2fa4c
jdawg86 <jgudetti@ucsd.edu>
2021-02-13 22:04:42 -0800

    Completed part 3 with issues

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,40 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super()
+
+    const shadow = this.attachShadow({mode: 'open'});
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'product');
+
+    const img = document.createElement('png');
+    img.setAttribute('src','https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
+    img.setAttribute('alt','Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops')
+
+    const title = document.createElement('p');
+    title.setAttribute('class','title');
+    title.textContent = "Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops";
+
+    const price = document.createElement('p');
+    price.setAttribute('class','price');
+
+    const button = document.createElement('button');
+    button.setAttribute('onclick',"alert('Added to Cart!')");
+    this.compareDocumentPosition.textContent = "Add to Cart";
+
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', './styles/styles.css');
+
+    shadow.appendChild(li);
+    li.appendChild(linkElem);
+    li.appendChild(img);
+    li.appendChild(title);
+    li.appendChild(price);
+    li.appendChild(button);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
 // Script.js
+window.onload = function() {
+  console.log("ON LOAD")
+
+  var product = document.createElement('product-item')
+
+};
+
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  console.log("DOM loaded")
+
+  var store = window.localStorage;
+  var response;
+  if(store.getItem) {
+  response = fetch('https://fakestoreapi.com/products')
+    .then( response => {
+        response.json().then(data => {
+          store.setItem("response", JSON.stringify(response));
+        })
+        console.log("response " + response)
+    })
+  }
+
+
 });
